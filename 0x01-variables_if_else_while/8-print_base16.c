cf7ebf3af9744c97729d349702e91c7b9b612a8b
ObakengDPitse <obakeng.op@gmail.com>
2022-12-21 11:16:15 -0800

    print base 16 numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,13 @@ int main(void)
 {
 	char c;
 
-	for (c = 0; c < 16; c++)
+	for (c = 0; c < 10; c++)
 	{
 		putchar((c % 10) + '0');
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
