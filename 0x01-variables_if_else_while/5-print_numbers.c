e3fac0b386e8b37a7d3945c61ff99d1f7f6f262c
ObakengDPitse <obakeng.op@gmail.com>
2022-12-21 10:12:44 -0800

    print numbers 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 to 9
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
