69202410351d9dcaae6325cccb28d97ff58ffa94
ObakengDPitse <obakeng.op@gmail.com>
2022-12-21 11:30:10 -0800

    print digits separated by comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits
+ *
+ * Return: Always 0 
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar((c % 10) + '0');
+		if (c < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
