9429805ab10384c544f2c2e4bf7117d9c353c9ef
ObakengDPitse <obakeng.op@gmail.com>
2022-12-30 13:39:42 -0800

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabets
+ * @c: charecter input
+ *
+ * Return: 1 if is alpha, else 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (1);
+	}
+}
