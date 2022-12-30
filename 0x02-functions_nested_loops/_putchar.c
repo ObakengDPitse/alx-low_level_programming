fedad9518dd31c37f110e30f96fb7e5af7481c89
ObakengDPitse <obakeng.op@gmail.com>
2022-12-30 07:46:42 -0800

    putchar funtion

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes a chartecter 
+ * @c: charecter to be printed
+ *
+ * Return: Always 0
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
