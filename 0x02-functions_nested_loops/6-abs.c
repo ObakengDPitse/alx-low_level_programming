3ed32fdec8d0662cc84b2232ea1c34bd98f8df6c
ObakengDPitse <obakeng.op@gmail.com>
2022-12-30 14:05:26 -0800

    Absilute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - returns an absilute number
+ * @a: integer input
+ * Return: Absilute number
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (a * -1);
+}
