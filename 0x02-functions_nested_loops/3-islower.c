231de380028b47a9d7122d48498708abae811869
ObakengDPitse <obakeng.op@gmail.com>
2022-12-30 13:27:28 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check if alphabet is lower
+ * @c: charecter input
+ * Return: 1 if lower, else 0 
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
