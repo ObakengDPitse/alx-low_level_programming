346eb91ebb7105a4d097abcea2d0b83d0edcd03a
ObakengDPitse <obakeng.op@gmail.com>
2023-07-21 03:25:48 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,6 +24,18 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+	else if (c > b && b > a)
+	{
+		largest = c;
+	}
+	else if (a > c && c > b)
+	{
+		largest = a;
+	}
+	else
+	{
+		largest = b;
+	}
 
 	return (largest);
 }
