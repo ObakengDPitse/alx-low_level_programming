cc1dba0865d44800ff9a28036cec468ed47ac6f5
ObakengDPitse <obakeng.op@gmail.com>
2023-07-24 01:24:54 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,7 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 	int j = 0;
 
 	while (str[j] != '\0')
@@ -16,7 +16,7 @@ void puts2(char *str)
 
 	}
 
-	for (i = 0; i <= j; i += 2)
+	for (i = 0; i < j; i += 2)
 	{
 		_putchar(str[i]);
 	}
