032d5624f6c7d558d3280c0ad408242cba9752a5
ObakengDPitse <obakeng.op@gmail.com>
2023-08-24 06:28:37 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - prints before the main function
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
